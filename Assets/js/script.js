0c5b8238b640803981dc5aba4cfeaa514cdc6461
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-10 23:08:24 -0400

    added deletion functionality, created an autoload from history,  and cleaned up CSS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,8 +9,7 @@ const currentDay = mm + '/' + dd + '/' + yyyy;
 const formSubmitHandler = event => {
     event.preventDefault();
     
-    let city = cityName.value.trim();
-    console.log(city);
+    const city = cityName.value.trim();
   
     if (city) {
       getCityCoords(city);
@@ -21,72 +20,81 @@ const formSubmitHandler = event => {
   };
 
 const getCityCoords = city => {
-    let geoAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    const geoAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
 
     fetch(geoAPI)
         .then((response) => {
             if (response.ok) {
-            console.log(response);
-            response.json().then((data) => {  
-                appendStorage(data[0].name, data[0].lat, data[0].lon);
-                addCityButton(data[0].name, data[0].lat, data[0].lon);
-                generateCards(data[0].name, data[0].lat, data[0].lon);
+            response.json().then((data) => {
+                if (data.length > 0) {  
+                    appendStorage(data[0].name, data[0].lat, data[0].lon);
+                    addCityButton(data[0].name, data[0].lat, data[0].lon);
+                    generateCards(data[0].name, data[0].lat, data[0].lon);
+                } else {
+                    alert('Please enter a valid City Name');
+                }
             });
             } else {
-            alert('Error: ' + response.statusText);
+            console.error(response.statusText);
             }
         })
         .catch((error) => {
-            alert('Unable to connect to Open Weather');
+           console.error('Unable to connect to Open Weather', error);
         });
 };
 
 const addCityButton = (cityName, cLat, cLon) => {
-    let cityButtons = `<p class= "btn btn-block btn-light text-dark font-weight-bold" onclick="generateCards('${cityName}', ${cLat}, ${cLon})">${cityName}</p>`
-
+    const cityButtons = `<div class="row no-gutters my-3" id="${cityName}">` +
+                        `<div class="col-11">` +
+                            `<button class= "btn btn-block btn-light text-dark font-weight-bold" onclick="generateCards('${cityName}', ${cLat}, ${cLon})">${cityName}</button>` +
+                        `</div>` +
+                        `<div class="col-1">` +
+                            `<button class="btn btn-danger" onclick="deleteCity('${cityName}')">X</button>` +
+                        `</div>` +
+                      `</div>`; 
     document.getElementById("cityListings").innerHTML += cityButtons
 };
 
 const appendStorage = (cityName, cLat, cLon) => {
-    let cities = JSON.parse(localStorage.getItem("cities")) ?? [];
+    const cities = JSON.parse(localStorage.getItem("cities")) ?? [];
     cities.push({cName: cityName, lat: cLat, lon: cLon});
     localStorage.setItem("cities", JSON.stringify(cities)); 
 };
 
 const storageInit = () => {
-    let cities = JSON.parse(localStorage.getItem("cities")) ?? [];
+    const cities = JSON.parse(localStorage.getItem("cities")) ?? [];
     cities.forEach(city => {
         addCityButton(city.cName, city.lat, city.lon);
     });
 };
 
 const generateCards = (name, lat, lon) => {
-    let oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+    const oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
 
     fetch(oneCallAPI)
         .then((response) => {
             if (response.ok) {
             response.json().then((data) => {
-                console.log(data);
                 let current = data.current;
-                primaryCard(name, currentDay, current.weather[0].icon, current.temp, current.wind_speed, current.humidity, current.uvi);
+                createPrimaryCard(name, currentDay, current.weather[0].icon, current.temp, current.wind_speed, current.humidity, current.uvi);
+                chooseUVColor(current.uvi);
                 generateDeck(data.daily);
             });
             } else {
-            alert('Error: ' + response.statusText);
+            console.error('Error: ' + response.statusText);
             }
         })
         .catch((error) => {
-            alert('Unable to connect to Open Weather');
+            console.error('Unable to connect to Open Weather', error);
         });
 };
 
-const primaryCard = (city, date, emoji, temp, wind, humidity, uv) => {
-    let primary = `<h2 class="font-weight-bold card-title date">${city} (${date}) <img id="wicon" src="http://openweathermap.org/img/w/${emoji}.png" alt="Weather icon"></h2>` +
+const createPrimaryCard = (city, date, emoji, temp, wind, humidity, uv) => {
+    const primary = `<h2 class="font-weight-bold card-title date">${city} (${date}) <img id="wicon" src="http://openweathermap.org/img/w/${emoji}.png" alt="Weather icon"></h2>` +
                   `<p class="temp">Temp: ${temp}</p>` +
                   `<p class="wind">Wind: ${wind}</p>` +
                   `<p class="humidity">Humidity: ${humidity}</p>` +
-                  `<p id="uvIndex">UV Index: ${uv}</p>`;
+                  `<p>UV Index: <span id="uvIndex" class="text-white btn">${uv}</span></p>`;
     document.getElementById("currentCity").innerHTML = primary;
 };
 
@@ -95,19 +103,42 @@ const generateDeck = (data) => {
     document.getElementById("fiveDay").innerHTML = fiveDayCards;
     for (let i = 0; i < 5; i++) {
         fiveDayCards += `<div class="">` +
-                        `<div class="card bg-dark text-white p-3">` +
-                        `<h3 class="font-weight-bold date">${mm + '/' + String(today.getDate() + parseInt([i])).padStart(2, '0') + '/' + yyyy}</h3>` +
-                        `<div class="card-body">` +
-                        `<p><img id="wicon" src="http://openweathermap.org/img/w/${data[i].weather[0].icon}.png" alt="Weather icon"></p>` +
-                        `<p class="temp">Temp: ${data[i].temp.day}</p>` +
-                        `<p class="wind">Wind: ${data[i].wind_speed}</p>` +
-                        `<p class="humidity">Humidity: ${data[i].humidity}</p>` +   
-                        `</div>` +
-                        `</div>` +
+                            `<div class="card bg-dark text-white p-3">` +
+                                `<h3 class="font-weight-bold date">${mm + '/' + String(today.getDate() + parseInt([i]) + 1).padStart(2, '0') + '/' + yyyy}</h3>` +
+                                `<div class="card-body">` +
+                                    `<p><img id="wicon" src="http://openweathermap.org/img/w/${data[i].weather[0].icon}.png" alt="Weather icon"></p>` +
+                                    `<p class="temp">Temp: ${data[i].temp.day}</p>` +
+                                    `<p class="wind">Wind: ${data[i].wind_speed}</p>` +
+                                    `<p class="humidity">Humidity: ${data[i].humidity}</p>` +   
+                                `</div>` +
+                            `</div>` +
                         `</div>`
     }
     document.getElementById("fiveDay").innerHTML += fiveDayCards;
+    document.getElementById("hideBlock").classList.remove("d-none");
+};
+
+const chooseUVColor = uv => {
+    const uvColor = document.getElementById("uvIndex");
+    uv < 4 ? uvColor.classList.add("bg-success") :
+    uv > 8 ? uvColor.classList.add("bg-danger") :
+    uvColor.classList.add("bg-warning");
+};
+
+const deleteCity = cityName => {
+    const cities = JSON.parse(localStorage.getItem("cities"));
+    const newCities = cities.filter(city => city.cName !== cityName);
+    localStorage.setItem("cities", JSON.stringify(newCities));
+    document.getElementById(cityName).remove();
+};
+
+const loadFirstCity = () => {
+    const cities = JSON.parse(localStorage.getItem("cities")) ?? [];
+    if (cities.length > 0) {
+        generateCards(cities[0].cName, cities[0].lat, cities[0].lon)
+    }
 };
 
 storageInit();
+loadFirstCity();
 cityForm.addEventListener('submit', formSubmitHandler);
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# weather
-Weather Information based on user selected criteria
+# Weather Dashboard
+A simple weather dashboard that uses openweatherapi to grab a weather forecast.
+
+## HTML / CSS / JavaScript
+The dashboard takes a user submitted city and programatically creates a weather forecast using API calls
+
+## Deployed Site
+[Weather Dashboard](https://josephlmurray.github.io/weather/)
+
+## Screen Shot
+
+![alt text](./Assets/weatherdashboard.png "Weather Dashboard")
+
+### Points of Interest
+* No data is shown if no search history exists
+
+* If search history exists, the page automatically pulls information for the top most city
+
+* All cards are generated programatically 
